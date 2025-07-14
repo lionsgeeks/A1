14bfdb18e3e91b121d4cef0122f103662ed06f7e
Ayman Boujjar <aymenboujjar12@gmail.com>
2025-07-14 12:23:26 +0100

    modifications in the logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,17 @@
-import AppLogoIcon from './app-logo-icon';
+import logo from '../../assets/images/A1.png';
 
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-[#dadada] text-sidebar-primary-foreground">
-                <AppLogoIcon className=" fill-current " />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-white/10 overflow-hidden">
+                <img
+                    src={logo}
+                    alt="A1 Logo"
+                    className="w-full h-full object-contain"
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left ">
-                <span className="mb-0.5 truncate leading-tight font-semibold text-xl">Atelier 1</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold text-xl text-primary">ARCH Studio</span>
             </div>
         </>
     );
